6cf836464a179bdec50a9cd9358f36e8eff222e7
arunacharya1603 <arunacharya1603@gmail.com>
2024-06-05 20:17:50 +0530

    update map

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,20 @@ const Footer = () => {
   return (
     <footer className="bg-gray-300 py-6">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center">
-          <p className="text-gray-700">© 2024 Arun. All rights reserved.</p>
+        <div className="flex flex-col sm:flex-row justify-between items-center">
+          <p className="text-gray-700 mb-4 sm:mb-0">
+            © 2024 Arun. All rights reserved.
+          </p>
           <div className="flex space-x-4">
-            <Link to="/gallary" className="text-gray-700 hover:text-gray-900">Gallary</Link>
-            <Link to="#" className="text-gray-700 hover:text-gray-900">Acheivments</Link>
-            <Link to="contact" className="text-gray-700 hover:text-gray-900">Contact Us</Link>
+            <Link to="/gallary" className="text-gray-700 hover:text-gray-900">
+              Gallery
+            </Link>
+            <Link to="#" className="text-gray-700 hover:text-gray-900">
+              Achievements
+            </Link>
+            <Link to="/contact" className="text-gray-700 hover:text-gray-900">
+              Contact Us
+            </Link>
           </div>
         </div>
       </div>
--- src/components/MapDetails.jsx
+++ src/components/MapDetails.jsx
@@ -4,17 +4,19 @@ const MapDetails = () => {
   return (
     <>
       <div className="flex flex-col justify-center items-center">
-        <p className="text-3xl font-bold text-emerald-500 mb-4 mt-4 box-border shadow-md shadow-emerald-400 drop-shadow-xl rounded-md">Map</p>
-        <address className="text-2xl font-semibold text-orange-950 mb-4">Karaundi</address>
-        <div>
+        <p className="text-3xl font-bold text-emerald-500 mb-4 mt-4 box-border shadow-md shadow-emerald-400 drop-shadow-xl rounded-md">
+          Map
+        </p>
+        <address className="text-2xl font-semibold text-orange-950 mb-4">
+          Karaundi
+        </address>
+        <div className="p-4">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1294.7604121220331!2d83.73546279733016!3d26.379539593627857!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3993d1f84fe8c167%3A0x9e59e53c9a35abc4!2sRamjanki%20Mandir!5e0!3m2!1sen!2sin!4v1712993131871!5m2!1sen!2sin"
-            width="400"
-            height="250"
+            className="w-full h-64 sm:h-72 md:h-96 rounded-md shadow-lg"
             allowFullScreen=""
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
-            className="rounded-md shadow-lg"
           ></iframe>
         </div>
       </div>
